ada5da0090f4737c6aa6b7c5a283f30982df71bb
lql <651250447@qq.com>
2019-04-06 17:35:45 +0800

    完成分类数据加载

--- src/pages/category/index.vue
+++ src/pages/category/index.vue
@@ -2,13 +2,37 @@
     <div>
         <!-- 搜索条 -->
         <search-bar></search-bar>
+        <!-- 菜单和内容 -->
+        <div class="content">
+            <div class="left"></div>
+            <div class="right"></div>
+        </div>
     </div>
 </template>
 <script>
 import searchBar from '../../components/searchBar'
+import request from '../../utils/request'
 export default {
+  data () {
+    return {
+      cate: []
+    }
+  },
+  methods: {
+    async cateData () {
+      let ret = await request('categories')
+      this.cate = ret.data.message
+      console.log(ret)
+    }
+  },
+  mounted () {
+    this.cateData()
+  },
   components: {
     'search-bar': searchBar
   }
 }
 </script>
+<style lang="scss" scoped>
+  @import 'main.scss';
+</style>
